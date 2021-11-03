ce06c5bf0ef9f2b021d6e5c88413aba64d294fbd
Michael Orr <mike@glquote.com>
2021-11-03 11:12:26 -0700

    seeds are working

--- models/Professional.js
+++ models/Professional.js
@@ -1,14 +1,8 @@
 const { Model, DataTypes } = require("sequelize");
-const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
 // create our Professional model
-class Professional extends Model {
-  // set up method to run on instance data (per user) to check password
-  checkPassword(loginPw) {
-    return bcrypt.compare(loginPw, this.password);
-  }
-}
+class Professional extends Model {}
 
 Professional.init(
   {
@@ -28,20 +22,6 @@ Professional.init(
     }
   },
   {
-    hooks: {
-      // set up beforeCreate lifecycle "hook" functionality
-      beforeCreate: async (newProfessionalData) => {
-        newProfessionalData.password = await bcrypt.hash(newProfessionalData.password, 10);
-        return newProfessionalData;
-      },
-      beforeUpdate: async (newProfessionalData) => {
-        newProfessionalData.password = await bcrypt.hash(
-          newProfessionalData.password,
-          10
-        );
-        return newProfessionalData;
-      },
-    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
