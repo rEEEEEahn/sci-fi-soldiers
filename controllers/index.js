097056f02dda179085178de75bee976b2c7ad33a
Michael Orr <mike@glquote.com>
2021-11-04 09:53:56 -0700

    outline for routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const homeRouter = require('./home-router');
-const apiRouter = require('./api');
-const professionalsRouter = require('./professional');
+const apiRouter = require('./api-router');
 router.use('/', homeRouter);
 router.use('/api', apiRouter);
-router.use('/professionals', professionalsRouter);
 module.exports = router;
--- controllers/professional/index.js
+++
@@ -1,4 +0,0 @@
-const router = require('express').Router();
-const professionalsRouter = require('./professionals-router');
-router.use('/', professionalsRouter);
-module.exports = router;
